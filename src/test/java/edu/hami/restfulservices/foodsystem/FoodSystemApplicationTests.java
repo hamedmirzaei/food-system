b956a400c1096f3c8656f2b9705e1d707a8542a7
Hamed Mirzaei <mirzaeihamed1368@gmail.com>
2023-03-19 14:44:35 -0600

    The skeletone of the project builf by spring initilizer

---
+++ src/test/java/edu/hami/restfulservices/foodsystem/FoodSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hami.restfulservices.foodsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
